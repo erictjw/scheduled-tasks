34dda15992ec7a9fcdc860dab956faea61a49196
erictjw <86952450+erictjw@users.noreply.github.com>
2026-05-26 03:02:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,23 +16,33 @@ import os
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
+today = dt.datetime.today()
+today_month = today.month
+today_day = today.day
+today = (today_month, today_day)
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if ((today) in birthday_dict):
+    birthday_email = birthday_dict[today]["email"]
+    birthday_name = birthday_dict[today]["name"]
+
+letter_list = random.randint(1,3)
+letter = f"./letter_templates/letter_{letter_list}.txt"
+with open(letter) as letter_file:
+    letter = letter_file.read()
+    letter = letter.replace("[NAME]", birthday_name)
+
+my_email = "infotoerictangallery@gmail.com"
+password = "ubetcnqvnrtoyvtg"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL,
+                        to_addrs={birthday_email},
+                        msg=f"Subject:Happy birthday {birthday_name}\n\n{letter}"
+                        )
+
+
