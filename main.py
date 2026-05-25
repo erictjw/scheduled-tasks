95cb3fef3a44d6788c1c6d48d9b8df317b5e1bdf
erictjw <86952450+erictjw@users.noreply.github.com>
2026-05-26 03:31:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,49 +6,49 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-print("🔥 SCRIPT IS EXECUTING")
-import sys
-sys.stdout.flush()
+# print("🔥 SCRIPT IS EXECUTING")
+# import sys
+# sys.stdout.flush()
 
-# from datetime import datetime
-# import pandas
-# import random
-# import smtplib
-# import os
+from datetime import datetime
+import pandas
+import random
+import smtplib
+import os
 
-# # import os and use it to get the Github repository secrets
-# MY_EMAIL = os.environ.get("MY_EMAIL")
-# MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# import os and use it to get the Github repository secrets
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-# today = datetime.today()
-# today_month = today.month
-# today_day = today.day
-# today = (today_month, today_day)
-# data = pandas.read_csv("birthdays.csv")
+today = datetime.today()
+today_month = today.month
+today_day = today.day
+today = (today_month, today_day)
+data = pandas.read_csv("birthdays.csv")
 
-# birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-# if ((today) in birthday_dict):
-#     birthday_email = birthday_dict[today]["email"]
-#     birthday_name = birthday_dict[today]["name"]
+if ((today) in birthday_dict):
+    birthday_email = birthday_dict[today]["email"]
+    birthday_name = birthday_dict[today]["name"]
 
-#     print(birthday_email)
+    print(birthday_email)
     
-#     letter_list = random.randint(1,3)
-#     letter = f"./letter_templates/letter_{letter_list}.txt"
-#     with open(letter) as letter_file:
-#         letter = letter_file.read()
-#         letter = letter.replace("[NAME]", birthday_name)
+    letter_list = random.randint(1,3)
+    letter = f"./letter_templates/letter_{letter_list}.txt"
+    with open(letter) as letter_file:
+        letter = letter_file.read()
+        letter = letter.replace("[NAME]", birthday_name)
     
-#     my_email = "infotoerictangallery@gmail.com"
-#     password = "ubetcnqvnrtoyvtg"
+    my_email = "infotoerictangallery@gmail.com"
+    password = "ubetcnqvnrtoyvtg"
     
-#     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-#         connection.starttls()
-#         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-#         connection.sendmail(from_addr=MY_EMAIL,
-#                             to_addrs={MY_EMAIL},
-#                             msg=f"Subject:Happy birthday {birthday_name}\n\n{letter}"
-#                             )
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_email,
+                            msg=f"Subject:Happy birthday {birthday_name}\n\n{letter}"
+                            )
 
 
